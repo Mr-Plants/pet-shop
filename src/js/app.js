7f488e90e13daa4388eacc295c138e2d7b8ba0d2
weiyabo1 <weiyabo1@jd.com>
2022-11-08 10:59:23 +0800

    完善demo

--- src/js/app.js
+++ src/js/app.js
@@ -1,68 +1,80 @@
-App = {
-  web3Provider: null,
-  contracts: {},
 
-  init: async function() {
-    // Load pets.
-    $.getJSON('../pets.json', function(data) {
-      var petsRow = $('#petsRow');
-      var petTemplate = $('#petTemplate');
+const App = {
+    web3Provider: null, contracts: {},
 
-      for (i = 0; i < data.length; i ++) {
-        petTemplate.find('.panel-title').text(data[i].name);
-        petTemplate.find('img').attr('src', data[i].picture);
-        petTemplate.find('.pet-breed').text(data[i].breed);
-        petTemplate.find('.pet-age').text(data[i].age);
-        petTemplate.find('.pet-location').text(data[i].location);
-        petTemplate.find('.btn-adopt').attr('data-id', data[i].id);
+    init: async function () {
+        // Load pets.
+        $.getJSON('../pets.json', function (data) {
+            let petsRow = $('#petsRow');
+            let petTemplate = $('#petTemplate');
 
-        petsRow.append(petTemplate.html());
-      }
-    });
+            for (let i = 0; i < data.length; i++) {
+                petTemplate.find('.panel-title').text(data[i].name);
+                petTemplate.find('img').attr('src', data[i].picture);
+                petTemplate.find('.pet-breed').text(data[i].breed);
+                petTemplate.find('.pet-age').text(data[i].age);
+                petTemplate.find('.pet-location').text(data[i].location);
+                petTemplate.find('.btn-adopt').attr('data-id', data[i].id);
 
-    return await App.initWeb3();
-  },
+                petsRow.append(petTemplate.html());
+            }
+        });
 
-  initWeb3: async function() {
-    /*
-     * Replace me...
-     */
+        return await App.initWeb3();
+    },
 
-    return App.initContract();
-  },
+    initWeb3: async function () {
+        if (typeof window.ethereum !== "undefined") {
+            App.web3Provider = window.Web3.givenProvider;
+        } else {
+            App.web3Provider = new Web3.providers.WebsocketProvider('ws://localhost:7545');
+        }
 
-  initContract: function() {
-    /*
-     * Replace me...
-     */
+        return App.initContract();
+    },
 
-    return App.bindEvents();
-  },
+    initContract: function () {
+        $.getJSON('Adoption.json', data => {
+            App.contracts.Adoption = TruffleContract(data);
+            App.contracts.Adoption.setProvider(App.web3Provider);
+            // 初始化领养标记
+            return App.markAdopted();
+        })
 
-  bindEvents: function() {
-    $(document).on('click', '.btn-adopt', App.handleAdopt);
-  },
+        return App.bindEvents();
+    },
 
-  markAdopted: function() {
-    /*
-     * Replace me...
-     */
-  },
+    bindEvents: function () {
+        $(document).on('click', '.btn-adopt', App.handleAdopt);
+    },
 
-  handleAdopt: function(event) {
-    event.preventDefault();
+    markAdopted: async () => {
+        const instance = await App.contracts.Adoption.deployed();
+        const adopters = await instance.getAdopters.call();
+        for (let i = 0; i < adopters.length; i++) {
+            if (adopters[i] !== "0x0000000000000000000000000000000000000000") {
+                //    第i个被领养了
+                $('.panel-pet').eq(i).find('button').text('success').attr('disabled', true);
+            }
+        }
+    },
 
-    var petId = parseInt($(event.target).data('id'));
+    handleAdopt: async (event) => {
+        event.preventDefault();
 
-    /*
-     * Replace me...
-     */
-  }
+        let petId = parseInt($(event.target).data('id'));
+
+        // const accounts = await Web3.eth.getAccounts();
+        const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+        const instance = await App.contracts.Adoption.deployed();
+        await instance.adopt(petId, {from: accounts[0]});
+        App.markAdopted();  // refresh list
+    }
 
 };
 
-$(function() {
-  $(window).load(function() {
-    App.init();
-  });
+$(function () {
+    $(window).load(function () {
+        App.init();
+    });
 });
